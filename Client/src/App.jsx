52cf74ef3d8b37f17d803f2437ab6939a0ec02b0
Devashish Dey <devashishdey01@gmail.com>
2026-09-23 17:07:21 +0530

    Update ServerUrl to production endpoint

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,7 +11,7 @@ import Builder from "./pages/Builder";
 import Billing from "./pages/Billing";
 import {Toaster} from 'react-hot-toast'
 
-export const ServerUrl = "http://localhost:8000";
+export const ServerUrl = "https://mern-aiserver.onrender.com";
 export const CLIENT_URL = "http://localhost:5173"
 
 const App = () => {
